4dd86756b1231718c9d21c63c9d4d21a3df87cbb
Anh Le <haianhltr@yahoo.com.vn>
2023-06-23 16:03:42 -0500

    define port number

--- main.tf
+++ main.tf
@@ -8,30 +8,36 @@ provider "aws" {
 # pass shell script to user data
 
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0" 
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
 
-    # script for instance to run
-    user_data =  <<-EOF
-        #!/bin/bash
-        echo "Hello World" > index.html
-        nohup busybox httpd -f -p 8080 &
-        EOF
-
-    tags = {
-        Name = "terraform-example"
-    }
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
-# create a security group to allow incoming/outgoing traffic from EC2 Instances
-resource "aws_security_group" "instance" { 
-    name = "terraform-example-instance"
-    ingress {
-        from_port =8080
-        to_port = 8080 
-        protocol = "tcp" 
-        cidr_blocks = ["0.0.0.0/0"]
-} 
+resource "aws_security_group" "instance" {
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# define port number
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
 }
