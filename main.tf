e0935840ed7fbf916cc63b7b5ac1da77bdf99360
Anh Le <haianhltr@yahoo.com.vn>
2023-06-23 12:28:51 -0500

    simple script for a single web server

--- main.tf
+++ main.tf
@@ -5,10 +5,18 @@ provider "aws" {
 }
 
 # free AMI
+# pass shell script to user data
 
 resource "aws_instance" "example" {
     ami = "ami-0c55b159cbfafe1f0" 
     instance_type = "t2.micro"
+
+
+    user_data =  <<-EOF
+        #!/bin/bash
+        echo "Hellom World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
 
     tags = {
         Name = "terraform-example"
