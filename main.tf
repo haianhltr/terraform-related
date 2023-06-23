c6e88c6b8551d0e5cb762c721d903c82fba4385a
Anh Le <haianhltr@yahoo.com.vn>
2023-06-23 12:20:32 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+# deploy single virtual server in aws
+
+provider "aws" { 
+    region = "us-east-2"
+}
+
+# free AMI
+
+resource "aws_instance" "example" {
+    ami = "ami-0c55b159cbfafe1f0" 
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
